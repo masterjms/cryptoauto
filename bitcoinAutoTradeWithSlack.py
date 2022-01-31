bd39d0997056807cd2f387b14e1ba2ef4b3c584b
Jang Min Sung <mstoto0319@gmail.com>
2022-01-31 21:11:16 +0900

    슬랙 메세지 수정

--- bitcoinAutoTradeWithSlack.py
+++ bitcoinAutoTradeWithSlack.py
@@ -67,12 +67,12 @@ while True:
                 krw = get_balance("KRW")
                 if krw > 5000:
                     buy_result = upbit.buy_market_order("KRW-BTC", krw*0.9995)
-                    post_message(myToken,"#crypto", "BTC buy : " +str(buy_result)) 
+                    post_message(myToken,"#crypto", "비트코인 매수 : " +str(buy_result)) 
         else:
             btc = get_balance("BTC")
             if btc > 0.00008:
                 sell_result = upbit.sell_market_order("KRW-BTC", btc*0.9995)
-                post_message(myToken,"#crypto", "BTC buy : " +str(sell_result))
+                post_message(myToken,"#crypto", "비트코인 매도 : " +str(sell_result))
         time.sleep(1)
     
     except Exception as e:
